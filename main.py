790a671aab584cff327fdefdd9c11f3a118e571c
jakhongirs <j.sagdullaev31@gmail.com>
2022-07-14 13:48:39 +0500

    grabbing an image and download it

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import requests
 import bs4
 
-# GRABING PAGE TITLE:
+# GRABBING PAGE TITLE:
 result = requests.get("https://example.com/")
 soup = bs4.BeautifulSoup(result.text, "lxml")
 page_title = soup.select('title')[0].getText()
 
-# GRABING All ELEMENTS OF A CLASS:
+# GRABBING All ELEMENTS OF A CLASS:
 res = requests.get("https://en.wikipedia.org/wiki/FC_Nasaf")
 soup2 = bs4.BeautifulSoup(res.text, "lxml")
 
 players_list = [player.getText() for player in soup2.select('.fn')]
 
+# GRABBING AN IMAGE:
+response = requests.get("https://en.wikipedia.org/wiki/Deep_Blue_(chess_computer)")
+soup3 = bs4.BeautifulSoup(response.text, "lxml")
+
+computer_img = soup3.select(".thumbimage")[0]
+image_link = requests.get(f"https:{computer_img['src']}")
+
+f = open('/Users/jakhongir/desktop/computer.jpg', 'wb')
+f.write(image_link.content)
+f.close()
 
