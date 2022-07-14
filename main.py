0d2bc8ed2f456d2134c7ed54a2ecf43c26b0480e
jakhongirs <j.sagdullaev31@gmail.com>
2022-07-14 14:16:28 +0500

    WORKING WITH MULTIPLE PAGES AND ITEMS

--- main.py
+++ main.py
@@ -23,3 +23,24 @@ f = open('/Users/jakhongir/desktop/computer.jpg', 'wb')
 f.write(image_link.content)
 f.close()
 
+# WORKING WITH MULTIPLE PAGES AND ITEMS:
+
+# TODO: Get title of every book with a 2 star rating.
+base_url = "http://books.toscrape.com/catalogue/page-{}.html"
+
+book_res = requests.get(base_url.format(1))
+soup4 = bs4.BeautifulSoup(book_res.text, "lxml")
+products = soup4.select(".product_pod")
+
+two_star_titles = []
+
+for n in range(1, 51):
+    scrape_url = base_url.format(n)
+    res = requests.get(scrape_url)
+    soup = bs4.BeautifulSoup(res.text, "lxml")
+    books = soup.select(".product_pod")
+
+    for book in books:
+        if len(book.select(".star-rating.Two")) != 0:
+            book_title = book.select('a')[1]["title"]
+            two_star_titles.append(book_title)
