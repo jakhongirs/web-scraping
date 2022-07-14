310a75c7e76fbb4672d727a96d1dd4d198aecf7d
jakhongirs <j.sagdullaev31@gmail.com>
2022-07-14 12:32:57 +0500

    grabbing all elements of a class

--- main.py
+++ main.py
@@ -6,4 +6,10 @@ result = requests.get("https://example.com/")
 soup = bs4.BeautifulSoup(result.text, "lxml")
 page_title = soup.select('title')[0].getText()
 
-print(page_title)
+# GRABING All ELEMENTS OF A CLASS:
+res = requests.get("https://en.wikipedia.org/wiki/FC_Nasaf")
+soup2 = bs4.BeautifulSoup(res.text, "lxml")
+
+players_list = [player.getText() for player in soup2.select('.fn')]
+
+
